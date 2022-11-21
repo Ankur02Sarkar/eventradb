95eeaa082b7f904ca0be61d00ba794ed4b42103f
Ankur Sarkar <sarkarrohonankur@gmail.com>
2022-11-21 12:37:19 +0530

    update

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const apiData = require("./data.json");
 app.get("/", (req, res) => {
   res.send("Live");
 });
-app.get("/service", (req, res) => {
+app.get("/api", (req, res) => {
   res.send(apiData);
 });
 app.listen(port, () => {
