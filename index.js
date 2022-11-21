4089cbd6ff197738e21e749bf8e9f27e79665a22
Ankur Sarkar <sarkarrohonankur@gmail.com>
2022-11-21 12:35:45 +0530

    update

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(cors());
+const port = process.env.PORT || 3001;
+const apiData = require("./data.json");
+app.get("/", (req, res) => {
+  res.send("Live");
+});
+app.get("/service", (req, res) => {
+  res.send(apiData);
+});
+app.listen(port, () => {
+  console.log("Live on console");
+});
